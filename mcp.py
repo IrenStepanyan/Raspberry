fa8146047cde46154600048e494fa0d461e8b789
IrenStepanyan <i23.iren.stepanyan@etud.ufar.am>
2025-04-09 16:27:23 +0100

    2

--- mcp.py
+++ mcp.py
@@ -2,9 +2,9 @@ import spidev
 import RPi.GPIO as GPIO
 import time
 
-# Constants
-VREF = 3.3  # Voltage reference (usually 3.3V on Raspberry Pi)
-LED_PIN = 18
+
+VREF = 3.3  
+#LED_PIN = 18
 
 # Setup SPI
 spi = spidev.SpiDev()
@@ -13,9 +13,9 @@ spi.max_speed_hz = 1350000
 
 # Setup PWM pin
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
-pwm = GPIO.PWM(LED_PIN, 1000)
-pwm.start(0)
+#GPIO.setup(LED_PIN, GPIO.OUT)
+#pwm = GPIO.PWM(LED_PIN, 1000)
+#pwm.start(0)
 
 def read_channel(channel):
     adc = spi.xfer2([1, (8 + channel) << 4, 0])
@@ -24,17 +24,22 @@ def read_channel(channel):
 
 try:
     while True:
-        adc_value = read_channel(0)
-        voltage = (adc_value / 1023.0) * VREF
-        pwm.ChangeDutyCycle((adc_value / 1023.0) * 100)
+        pwm_value = read_channel(0)
+        ldr_value = read_channel(1)
+        voltage = (pwm_value / 1023.0) * VREF
+        voltage1 = (ldr_value / 1023.0) * VREF
+
+        #pwm.ChangeDutyCycle((pwm_value / 1023.0) * 100)
         
-        print(f"ADC Value: {adc_value}, Voltage: {voltage:.2f} V")
+        print(f"PWM: ADC Value: {pwm_value}, Voltage: {voltage:.2f} V")
+        
+        print(f"LRD: ADC Value: {ldr_value}, Voltage: {voltage1: .2f} V")
         time.sleep(0.1)
 
 except KeyboardInterrupt:
     print("Exiting...")
 finally:
-    pwm.stop()
+    #pwm.stop()
     GPIO.cleanup()
     spi.close()
 
